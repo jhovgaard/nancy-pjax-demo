4922399d72ce0c17b85d0c5259dc2f281c9cfb5c
Jonas Hovgaard <j@jhovgaard.dk>
2014-04-23 17:41:02 +0200

    Initial

---
+++ NancyPjaxDemo/HomeModule.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Nancy;
+
+namespace NancyPjaxDemo
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Before += ctx =>
+            {
+                ViewBag.IsPjax = Request.Headers.Keys.Contains("X-PJAX");
+                return null;
+            };
+
+            Get["/"] = p =>
+            {
+                return View["Index"];
+            };
+
+            Get["/first/"] = p =>
+            {
+                return View["First"];
+            };
+
+            Get["/second/"] = p =>
+            {
+                return View["Second"];
+            };
+        }
+    }
+}
